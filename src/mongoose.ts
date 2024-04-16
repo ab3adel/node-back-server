ca61d482051490c72e2d9199b15a25fb6acec969
sgambelluri <andrea.sgambelluri@cnit.it>
2024-04-16 15:12:07 +0300

    MongoDB string connection

--- src/mongoose.ts
+++ src/mongoose.ts
@@ -1,7 +1,8 @@
 import mongoose, { Mongoose } from 'mongoose';
 import logger from './logger';
 //etMEmPCgLqQovT8j
-export const MONGODB_URL= `mongodb+srv://mohammadismael78:etMEmPCgLqQovT8j@cluster0.l5on1r8.mongodb.net/?retryWrites=true&w=majority`
+
+export const MONGODB_URL= `mongodb+srv://MohammadIsmaeel:etMEmPCgLqQovT8j@cluster0.31pbuck.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 const test ='mongodb://127.0.0.1:27017'
 export default  async function () :Promise<any>{
   console.log ('start connecting')
